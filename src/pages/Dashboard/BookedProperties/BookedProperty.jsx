a1933499014d6e9f726dc405b1cef9d2f35a2c88
tezalve <tezalve@gmail.com>
2024-09-03 04:28:35 +0600

    :money_with_wings: bkash payment

--- src/pages/Dashboard/BookedProperties/BookedProperty.jsx
+++ src/pages/Dashboard/BookedProperties/BookedProperty.jsx
@@ -1,5 +1,6 @@
 import { faMoneyBill, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -29,56 +30,14 @@ const BookedProperty = ({ property }) => {
         }
     }
 
-    bKash.init({
-        //options -
-        //1) 'checkout' : Performs a single checkout.
-        paymentMode: 'checkout',
-
-        //paymentRequest format : { amount: _AMOUNT, intent: _INTENT };
-        //intent : sale - immediate trx
-        //intent : authorization - needs an additional 'Capture' call for trx
-        //max two decimal points allowed in amount value.
-        //paymentRequest will be ignored for paymentMode 'addWalletOnly'. Just keep it as blank object (paymentRequest:{})
-        paymentRequest: { amount: '100.50', intent: 'sale' },
-
-        createRequest: function (request) {
-
-
-            //CALL YOUR BACKEND'S CREATE METHOD HERE
-            //IF THE CALL IS SUCCESSFUL, SEND THE CREATE RESPONSE DATA IN bKash.create().onSuccess() METHOD
-                bKash.create().onSuccess(createResponseData);
-            //ELSE, CALL bKash.create().onError() METHOD. bKash will run it's clean up code from there
-                bKash.create().onError();
-
-        },
-        executeRequestOnAuthorization: function () {
-
-
-            //CALL YOUR BACKEND'S EXECUTE METHOD HERE
-            //IF THE CALL IS SUCCESSFUL, DISPLAY YOUR SUCCESS PAGE
-                window.location.href = "your_success_page.html";
-            //ELSE, CALL bKash.execute().onError() METHOD. bKash will run it's clean up code from there
-                bKash.execute().onError();
-
-        },
-        onClose : function () {
-            //define what happens if the user closes the pop up window
-
-            //your code goes here
+    const pay = async () => {
+        try {
+            const { data } = await axios.post('http://localhost:5000/api/bkash/payment/create', { amount: 50, orderId: 1 }, { withCredentials: true })
+            window.location.href = data.bkashURL
+        } catch (error) {
+            console.log(error.response.data)
         }
-    });
-
-
-    //OPTIONAL function
-    //AFTER CALLING bKash.init, you can call bKash.reconfigure method according to your needs
-
-    //    bKash.reconfigure({
-    //      paymentMode: 'newPaymentMode',
-    //        paymentRequest: 'newPaymentRequest'
-    //    });
-
-    // NOTE : VIEW PAGE SOURCE TO SEE REAL LIFE IMPLEMENTATION
-    
+    }
 
     return (
         <div className='col-3 p-2'>
@@ -95,7 +54,7 @@ const BookedProperty = ({ property }) => {
                 <div className="card-body text-center border-right border-dark">
                     <Link to={`/dashboard/payment/${property._id}`} className={property.payment_done == false ? "btn" : "btn disabled"} ><FontAwesomeIcon className='fs-2 text-success' icon={faMoneyBill} /></Link>
                 </div>
-                <button id="bKash_button" disabled="disabled">Pay With bKash</button>
+                <button onClick={pay}>Pay With bKash</button>
             </div>
         </div>
     );
